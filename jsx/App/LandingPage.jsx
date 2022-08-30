95716b3065b88573fd4df8d4b53e9978c128c8e7
rbennett24 <rbennett.work@gmail.com>
2022-08-30 11:27:27 -0700

    Update LandingPage.jsx

--- jsx/App/LandingPage.jsx
+++ jsx/App/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ENGLISH, ESPANOL } from './locale/LocaleConstants.jsx';
+import { ENGLISH, ESPANOL , FRANCAIS } from './locale/LocaleConstants.jsx';
 import { TranslatableText } from './locale/TranslatableText.jsx'
 
 const landingPageJSX = {
@@ -24,6 +24,12 @@ const landingPageJSX = {
       <p>¡Bienvenidos! Este sitio comparte una colección de textos en <a href = https://es.wikipedia.org/wiki/Idioma_cachiquel>kaqchikel</a>, un idioma maya de la rama k'iche' que se habla en las tierras altas de Guatemala./p>
       <p>Estos textos constan de grabaciones de audio con anotaciones asociadas en formato <a href = https://archive.mpi.nl/tla/elan>ELAN</a>, y fueron producidos con LingView (<a href = https://github.com/BrownCLPS/LingView>https://github.com/BrownCLPS/LingView</a>).</p>
       <p>Un recordatorio para aquellos que participaron en este proyecto compartiendo el idioma y sus historias: si desea que hagamos algún cambio en su grabación o transcripción, incluida la eliminación del sitio, comuníquese con Ryan Bennett (rbennett@ucsc.edu).
+    </div>,
+  [FRANCAIS]:
+    <div>
+      <p>Bienvenue ! Ce site Web présente une collection de textes en <a href = https://en.wikipedia.org/wiki/Kaqchikel_language>Kaqchikel</a>, une langue maya de la branche k'ichean parlée dans les hautes terres du Guatemala.</p>
+      <p>Ces textes consistent en des enregistrements audio avec des annotations <a href = https://archive.mpi.nl/tla/elan>ELAN</a> associées, et ont été produits à l'aide de LingView (<a href = https:// github.com/BrownCLPS/LingView>https://github.com/BrownCLPS/LingView</a>).</p>
+      <p>Pour plus d'informations, veuillez contacter Ryan Bennett (rbennett@ucsc.edu)<p/>
     </div>,
 };
 
