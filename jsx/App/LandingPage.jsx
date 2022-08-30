6d605dc1c6682970367a38e659c4af5f56c2674c
rbennett24 <rbennett.work@gmail.com>
2022-08-30 15:30:46 -0700

    Update LandingPage.jsx

--- jsx/App/LandingPage.jsx
+++ jsx/App/LandingPage.jsx
@@ -26,9 +26,9 @@ const landingPageJSX = {
  Ütz ipetik! Ri ruxaq re' nuk'utu' jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, jun ch'ab'äl mayab' ntzijoxïk pa Iximulew.
  <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chre Ryan Bennett (rbennett@ucsc.edu).</p>
  <br/>
- <p>Welcome! This website presents a collection of texts in <a href="https://en.wikipedia.org/wiki/Kaqchikel_language">Kaqchikel</a>, a K'ichean-branch Mayan language spoken in the Guatemalan highlands.</p>
- <p>These texts consist of audio recordings with associated <a href="https://archive.mpi.nl/tla/elan">ELAN</a> annotations, and were produced using LingView (<a href="https://github.com/BrownCLPS/LingView">https://github.com/BrownCLPS/LingView</a>).</p>
- <p>For additional information, please contact Ryan Bennett (rbennett@ucsc.edu)</p>
+ <p>Bienvenue! Ce site web présente une collection de textes en <a href="https://fr.wikipedia.org/wiki/Cakchiquel_(langue)">Kaqchikel</a>, une langue maya de la branche k'ichean parlée dans les hautes terres du Guatemala.</p>
+ <p>Ces textes consistent en des enregistrements audio avec des annotations <a href="https://archive.mpi.nl/tla/elan">ELAN</a> associées, et ont été produits à l'aide de LingView (<a href=https://github.com/BrownCLPS/LingView>https://github.com/BrownCLPS/LingView</a>).</p>
+ <p>Pour plus d'informations, veuillez contacter Ryan Bennett (rbennett@ucsc.edu)</p>
  </div>,
 };
 
