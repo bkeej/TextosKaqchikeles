e8761a22ac305beee158d2eba020f5a4df438ef0
rbennett24 <rbennett.work@gmail.com>
2022-08-30 17:28:37 -0700

    Update LandingPage.jsx

--- jsx/App/LandingPage.jsx
+++ jsx/App/LandingPage.jsx
@@ -5,7 +5,7 @@ import { TranslatableText } from './locale/TranslatableText.jsx'
 const landingPageJSX = {
 [ENGLISH]:
  <div>
- Ütz ipetik! Ri ruxaq re' nuk'utu' jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, jun ch'ab'äl mayab' ntzijoxïk pa Iximulew.
+ Ütz ipetik! Ri xaq re' nuk'üt jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, jun mayab' ch'ab'äl nich'ab'aläx pan Iximulew.
  <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chre Ryan Bennett (rbennett@ucsc.edu).</p>
  <br/>
  <p>Welcome! This website presents a collection of texts in <a href="https://en.wikipedia.org/wiki/Kaqchikel_language">Kaqchikel</a>, a K'ichean-branch Mayan language spoken in the Guatemalan highlands.</p>
@@ -14,7 +14,7 @@ const landingPageJSX = {
  </div>,
  [ESPANOL]:
  <div>
- Ütz ipetik! Ri ruxaq re' nuk'utu' jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, jun ch'ab'äl mayab' ntzijoxïk pa Iximulew.
+ Ütz ipetik! Ri xaq re' nuk'üt jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, mayab' ch'ab'äl nich'ab'aläx pan Iximulew.
  <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chre Ryan Bennett (rbennett@ucsc.edu).</p>
  <br/>
  <p>¡Bienvenidos! Este sitio comparte una colección de textos en <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">kaqchikel</a>, un idioma maya de la rama k'iche' que se habla en las tierras altas de Guatemala.</p>
@@ -24,7 +24,7 @@ const landingPageJSX = {
  </div>,
  [FRANCAIS]:
  <div>
- Ütz ipetik! Ri ruxaq re' nuk'utu' jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, jun ch'ab'äl mayab' ntzijoxïk pa Iximulew.
+ Ütz ipetik! Ri xaq re' nuk'üt jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, mayab' ch'ab'äl nich'ab'aläx pan Iximulew.
  <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chre Ryan Bennett (rbennett@ucsc.edu).</p>
  <br/>
  <p>Bienvenue! Ce site web présente une collection de textes en <a href="https://fr.wikipedia.org/wiki/Cakchiquel_(langue)">Kaqchikel</a>, une langue maya de la branche k'ichean parlée dans les hautes terres du Guatemala.</p>
