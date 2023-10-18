cd4304c5a7459c5a1718b0e22c6d47984ca34d38
Robert Henderson <balamkej@gmail.com>
2023-10-18 14:02:17 -0700

    add henderson contact info

--- jsx/App/LandingPage.jsx
+++ jsx/App/LandingPage.jsx
@@ -6,26 +6,26 @@ const landingPageJSX = {
 [ENGLISH]:
  <div>
  Ütz ipetik! Ri xaq re' nuk'üt jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, jun mayab' ch'ab'äl nich'ab'aläx pan Iximulew.
- <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chire ri a Ryan Bennett (rbennett@ucsc.edu).</p>
+ <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chire ri a Ryan Bennett (rbennett@ucsc.edu) o ri a Robert Henderson (rhenderson@arizona.edu).</p>
  <br/>
  <p>Welcome! This website presents a collection of texts in <a href="https://en.wikipedia.org/wiki/Kaqchikel_language">Kaqchikel</a>, a K'ichean-branch Mayan language spoken in the Guatemalan highlands.</p>
  <p>These texts consist of audio recordings with associated <a href="https://archive.mpi.nl/tla/elan">ELAN</a> annotations, and were produced using LingView (<a href="https://github.com/BrownCLPS/LingView">https://github.com/BrownCLPS/LingView</a>).</p>
- <p>For additional information, please contact Ryan Bennett (rbennett@ucsc.edu).</p>
+ <p>For additional information, please contact Ryan Bennett (rbennett@ucsc.edu) or Robert Henderson (rhenderson@arizona.edu).</p>
  </div>,
  [ESPANOL]:
  <div>
  Ütz ipetik! Ri xaq re' nuk'üt jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, mayab' ch'ab'äl nich'ab'aläx pan Iximulew.
- <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chire ri a Ryan Bennett (rbennett@ucsc.edu).</p>
+ <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chire ri a Ryan Bennett (rbennett@ucsc.edu) o ri a Robert Henderson (rhenderson@arizona.edu).</p>
  <br/>
  <p>¡Bienvenidos! Este sitio comparte una colección de textos en <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">kaqchikel</a>, un idioma maya de la rama k'iche' que se habla en las tierras altas de Guatemala.</p>
  <p>Estos textos constan de grabaciones de audio con anotaciones asociadas en formato <a href="https://archive.mpi.nl/tla/elan">ELAN</a>, y fueron producidos con LingView (<a href="https://github.com/BrownCLPS/LingView">https://github.com/BrownCLPS/LingView</a>).</p>
  <p>Para más información, comuníquese con Ryan Bennett (rbennett@ucsc.edu).</p>
- <p>Un recordatorio para aquellas personas quienes participaron en este proyecto compartiendo su idioma y sus historias: si desea que hagamos algún cambio en su grabación o transcripción, incluso que borremos esos materiales de este sitio, comuníquese con Ryan Bennett (rbennett@ucsc.edu).</p>
+ <p>Un recordatorio para aquellas personas quienes participaron en este proyecto compartiendo su idioma y sus historias: si desea que hagamos algún cambio en su grabación o transcripción, incluso que borremos esos materiales de este sitio, comuníquese con Ryan Bennett (rbennett@ucsc.edu) o Robert Henderson (rhenderson@arizona.edu).</p>
  </div>,
  [FRANCAIS]:
  <div>
  Ütz ipetik! Ri xaq re' nuk'üt jun molaj tzijonem pa <a href="https://es.wikipedia.org/wiki/Idioma_cachiquel">Kaqchikel</a> ch'ab'äl, mayab' ch'ab'äl nich'ab'aläx pan Iximulew.
- <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chire ri a Ryan Bennett (rbennett@ucsc.edu).</p>
+ <p>Wi nawajo' nanab'ej chik chi rij ri taq tzijonem re', tab'ana' utzil katz'ib'an chire ri a Ryan Bennett (rbennett@ucsc.edu) o ri a Robert Henderson (rhenderson@arizona.edu).</p>
  <br/>
  <p>Bienvenue! Ce site web présente une collection de textes en <a href="https://fr.wikipedia.org/wiki/Cakchiquel_(langue)">Kaqchikel</a>, une langue maya de la branche k'ichean parlée dans les hautes terres du Guatemala.</p>
  <p>Pour plus d'informations, veuillez contacter Ryan Bennett (rbennett@ucsc.edu).</p>
